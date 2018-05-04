5e32fc016e306bcb8e413cb9e7e0894a0b6ea021
coe <coe.app@gmail.com>
2018-05-04 11:34:00 +0900

    書き込みの受け取る

--- SimpleBle/storyboards/MainViewController.swift
+++ SimpleBle/storyboards/MainViewController.swift
@@ -21,7 +21,7 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
     
     
     let longDataServiceUuid = CBUUID(string: "D096F3C2-5148-410A-BA6A-20FEAD00D7CA")
-    let longDataWriteCharacteristicUuid = CBUUID(string: "D096F3C2-5148-410A-BA6A-20FEAD00D7CA")
+    let longDataWriteCharacteristicUuid = CBUUID(string: "E053BD84-1E5B-4A6C-AD49-C672A737880C")
     let longDataWriteLengthDescriptorUuid = CBUUID(string: "C4BDAB8A-BAC1-477A-925C-E1665553953C")
     
     var scanDataSource:ScanDataSource!
@@ -56,6 +56,11 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         scanDataSource = ScanDataSource()
         
+
+    }
+    
+    deinit {
+        peripheralManager.stopAdvertising()
     }
     
     // MARK: - CBCentralManagerDelegate
@@ -88,18 +93,22 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral)
     {
         print(#file,#function,#line)
-        
-
         connectedPeripheral = peripheral
         peripheral.delegate = self
         peripheral.discoverServices([longDataServiceUuid])
+
     }
     
     // MARK: - CBPeripheralDelegate
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?)
     {
+        print(#file,#function,#line)
         peripheral.services?.forEach({ (service) in
+            print(#file,#function,#line,"service.uuid:\(service.uuid)")
+            print(#file,#function,#line,"characteristics:\(service.characteristics?.count)")
+
             service.characteristics?.forEach({ (characteristic) in
+                print(#file,#function,#line,"characteristic.uuid:\(characteristic.uuid)")
                 if characteristic.uuid == longDataWriteCharacteristicUuid {
                     connectedPeripheralWriteCharacteristic = characteristic
                 }
@@ -115,11 +124,17 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
             //サービス、キャラクタリスティック追加はpoweredOnの後
             let service:CBMutableService = CBMutableService(type: longDataServiceUuid, primary: true)
             var characteristicsArray:[CBCharacteristic] = []
-            let characteristic = CBMutableCharacteristic(type: longDataWriteCharacteristicUuid, properties: .write, value: nil, permissions: .writeable)
+            let characteristic = CBMutableCharacteristic(type: longDataWriteCharacteristicUuid, properties: .writeWithoutResponse, value: nil, permissions: .writeable)
             
             characteristicsArray.append(characteristic)
             service.characteristics = characteristicsArray
             peripheralManager.add(service)
+            
+            //アドバタイジング開始
+            let option:[String : Any] = [
+                CBAdvertisementDataServiceUUIDsKey:[longDataServiceUuid]
+            ]
+            peripheralManager.startAdvertising(option)
             break
         default:
             break
@@ -127,9 +142,175 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
     }
     
     
+    /*!
+     *  @method peripheralManager:willRestoreState:
+     *
+     *  @param peripheral    The peripheral manager providing this information.
+     *  @param dict            A dictionary containing information about <i>peripheral</i> that was preserved by the system at the time the app was terminated.
+     *
+     *  @discussion            For apps that opt-in to state preservation and restoration, this is the first method invoked when your app is relaunched into
+     *                        the background to complete some Bluetooth-related task. Use this method to synchronize your app's state with the state of the
+     *                        Bluetooth system.
+     *
+     *  @seealso            CBPeripheralManagerRestoredStateServicesKey;
+     *  @seealso            CBPeripheralManagerRestoredStateAdvertisementDataKey;
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, willRestoreState dict: [String : Any]){
+        print(#file,#function,#line)
+
+    }
+    
+    
+    /*!
+     *  @method peripheralManagerDidStartAdvertising:error:
+     *
+     *  @param peripheral   The peripheral manager providing this information.
+     *  @param error        If an error occurred, the cause of the failure.
+     *
+     *  @discussion         This method returns the result of a @link startAdvertising: @/link call. If advertisement could
+     *                      not be started, the cause will be detailed in the <i>error</i> parameter.
+     *
+     */
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?){
+        print(#file,#function,#line)
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:didAddService:error:
+     *
+     *  @param peripheral   The peripheral manager providing this information.
+     *  @param service      The service that was added to the local database.
+     *  @param error        If an error occurred, the cause of the failure.
+     *
+     *  @discussion         This method returns the result of an @link addService: @/link call. If the service could
+     *                      not be published to the local database, the cause will be detailed in the <i>error</i> parameter.
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?){
+        print(#file,#function,#line,"\(error)")
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:central:didSubscribeToCharacteristic:
+     *
+     *  @param peripheral       The peripheral manager providing this update.
+     *  @param central          The central that issued the command.
+     *  @param characteristic   The characteristic on which notifications or indications were enabled.
+     *
+     *  @discussion             This method is invoked when a central configures <i>characteristic</i> to notify or indicate.
+     *                          It should be used as a cue to start sending updates as the characteristic value changes.
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic){
+        print(#file,#function,#line)
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:central:didUnsubscribeFromCharacteristic:
+     *
+     *  @param peripheral       The peripheral manager providing this update.
+     *  @param central          The central that issued the command.
+     *  @param characteristic   The characteristic on which notifications or indications were disabled.
+     *
+     *  @discussion             This method is invoked when a central removes notifications/indications from <i>characteristic</i>.
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic){
+        print(#file,#function,#line)
+
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:didReceiveReadRequest:
+     *
+     *  @param peripheral   The peripheral manager requesting this information.
+     *  @param request      A <code>CBATTRequest</code> object.
+     *
+     *  @discussion         This method is invoked when <i>peripheral</i> receives an ATT request for a characteristic with a dynamic value.
+     *                      For every invocation of this method, @link respondToRequest:withResult: @/link must be called.
+     *
+     *  @see                CBATTRequest
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest){
+        print(#file,#function,#line)
+
+    }
+    
+    
+    /*!
+     *  @method peripheralManagerIsReadyToUpdateSubscribers:
+     *
+     *  @param peripheral   The peripheral manager providing this update.
+     *
+     *  @discussion         This method is invoked after a failed call to @link updateValue:forCharacteristic:onSubscribedCentrals: @/link, when <i>peripheral</i> is again
+     *                      ready to send characteristic value updates.
+     *
+     */
+    func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager){
+        print(#file,#function,#line)
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:didPublishL2CAPChannel:error:
+     *
+     *  @param peripheral   The peripheral manager requesting this information.
+     *  @param PSM            The PSM of the channel that was published.
+     *  @param error        If an error occurred, the cause of the failure.
+     *
+     *  @discussion         This method is the response to a  @link publishL2CAPChannel: @/link call.  The PSM will contain the PSM that was assigned for the published
+     *                        channel
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didPublishL2CAPChannel PSM: CBL2CAPPSM, error: Error?){
+        print(#file,#function,#line)
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:didUnublishL2CAPChannel:error:
+     *
+     *  @param peripheral   The peripheral manager requesting this information.
+     *  @param PSM            The PSM of the channel that was published.
+     *  @param error        If an error occurred, the cause of the failure.
+     *
+     *  @discussion         This method is the response to a  @link unpublishL2CAPChannel: @/link call.
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didUnpublishL2CAPChannel PSM: CBL2CAPPSM, error: Error?){
+        print(#file,#function,#line)
+    }
+    
+    
+    /*!
+     *  @method peripheralManager:didOpenL2CAPChannel:error:
+     *
+     *  @param peripheral   The peripheral manager requesting this information.
+     *  @param requests     A list of one or more <code>CBATTRequest</code> objects.
+     *
+     *  @discussion         This method is invoked when <i>peripheral</i> receives an ATT request or command for one or more characteristics with a dynamic value.
+     *                      For every invocation of this method, @link respondToRequest:withResult: @/link should be called exactly once. If <i>requests</i> contains
+     *                      multiple requests, they must be treated as an atomic unit. If the execution of one of the requests would cause a failure, the request
+     *                      and error reason should be provided to <code>respondToRequest:withResult:</code> and none of the requests should be executed.
+     *
+     *  @see                CBATTRequest
+     *
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didOpen channel: CBL2CAPChannel?, error: Error?){
+        print(#file,#function,#line)
+    }
+    
+    
     private var receiverdData:Data = Data()
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest])
     {
+        print(#file,#function,#line)
         requests.forEach { (request) in
             switch request.characteristic.uuid {
             case longDataWriteCharacteristicUuid:
@@ -189,8 +370,8 @@ class MainViewController: UIViewController,CBCentralManagerDelegate,CBPeripheral
         guard let peripheral = self.connectedPeripheral else {
             return
         }
-        let data = UIImagePNGRepresentation(imageView.image!)
-        sendBytes(data: data!, peripheral: connectedPeripheral!, writeCharacteristic: connectedPeripheralWriteCharacteristic!,mtu:peripheral.maximumWriteValueLength(for: .withoutResponse))
+        let data = UIImagePNGRepresentation(imageView.image!)!
+        sendBytes(data: data, peripheral: peripheral, writeCharacteristic: connectedPeripheralWriteCharacteristic!,mtu:peripheral.maximumWriteValueLength(for: .withoutResponse))
     }
     
     private var datas:ArraySlice<Data> = ArraySlice()
